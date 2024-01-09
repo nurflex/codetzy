14049d49dba374804ac53f9b2957e3e13041e4d4
nurflex <138783966+nurflex@users.noreply.github.com>
2024-01-09 18:21:06 +0700

    Create acp.js

---
+++ acp.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+if (typeof document.onselectstart!="undefined") {
+document.onselectstart=new Function ("return false");
+}
+else{
+document.onmousedown=new Function ("return false");
+document.onmouseup=new Function ("return true");
+}
+</script >
