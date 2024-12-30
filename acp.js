55b8bc3c05e8f10ad097c78a8c0ae50c4f11e3c8
nurflex <138783966+nurflex@users.noreply.github.com>
2024-12-30 20:14:24 +0700

    Delete acp.js

--- acp.js
+++
@@ -1,7 +0,0 @@
-if (typeof document.onselectstart!="undefined") {
-document.onselectstart=new Function ("return false");
-}
-else{
-document.onmousedown=new Function ("return false");
-document.onmouseup=new Function ("return true");
-}
